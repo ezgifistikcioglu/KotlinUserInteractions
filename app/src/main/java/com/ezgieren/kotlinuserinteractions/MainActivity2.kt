e14be48dc5199b4e19f3c23c1a5931183000ee4e
Ezgi Fıstıkçıoğlu <fistikciogluezgi@gmail.com>
2023-05-23 14:42:26 +0300

    Event samples.
    onClickListener & onTouchListener

--- app/src/main/java/com/ezgieren/kotlinuserinteractions/MainActivity2.kt
+++ app/src/main/java/com/ezgieren/kotlinuserinteractions/MainActivity2.kt
@@ -1,6 +1,8 @@
 package com.ezgieren.kotlinuserinteractions
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.ezgieren.kotlinuserinteractions.databinding.Screen1Binding
 
@@ -13,5 +15,23 @@ class MainActivity2 : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.textViewText.text = getString(R.string.text)
+
+        //binding.button.setOnClickListener { View.OnClickListener{
+        //    Toast.makeText(this, "Step1", Toast.LENGTH_LONG).show()
+        //} }
+        //binding.button.setOnClickListener(buttonAlertOnClick)
+
+        window.decorView.rootView.setOnTouchListener(View.OnTouchListener { v, event ->
+            Toast.makeText(this, "Touched to screen", Toast.LENGTH_LONG).show()
+            return@OnTouchListener true
+        })
+    }
+    var buttonAlertOnClick = View.OnClickListener{
+        Toast.makeText(this, "Step2", Toast.LENGTH_LONG).show()
+    }
+
+    /// From xml
+    fun buttonAlertStep3(view: View) {
+        Toast.makeText(this, "Step3", Toast.LENGTH_LONG).show()
     }
 }
