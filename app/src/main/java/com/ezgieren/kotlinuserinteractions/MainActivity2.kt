494f342d1b6d1560d918e430f193921973487bc2
Ezgi Fıstıkçıoğlu <fistikciogluezgi@gmail.com>
2023-05-23 16:09:23 +0300

    Toggle&Switch, Checkbox&RadioButton Buttons Using

--- app/src/main/java/com/ezgieren/kotlinuserinteractions/MainActivity2.kt
+++ app/src/main/java/com/ezgieren/kotlinuserinteractions/MainActivity2.kt
@@ -2,6 +2,7 @@ package com.ezgieren.kotlinuserinteractions
 
 import android.os.Bundle
 import android.view.View
+import android.widget.CompoundButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.ezgieren.kotlinuserinteractions.databinding.Screen1Binding
@@ -19,13 +20,26 @@ class MainActivity2 : AppCompatActivity() {
         //binding.button.setOnClickListener { View.OnClickListener{
         //    Toast.makeText(this, "Step1", Toast.LENGTH_LONG).show()
         //} }
-        //binding.button.setOnClickListener(buttonAlertOnClick)
 
-        window.decorView.rootView.setOnTouchListener(View.OnTouchListener { v, event ->
-            Toast.makeText(this, "Touched to screen", Toast.LENGTH_LONG).show()
-            return@OnTouchListener true
-        })
+        binding.button.setOnClickListener(buttonAlertOnClick)
+
+        //window.decorView.rootView.setOnTouchListener(View.OnTouchListener { v, event ->
+        //    Toast.makeText(this, "Touched to screen", Toast.LENGTH_LONG).show()
+        //    return@OnTouchListener true
+        //})
+
+        // Toggle Button Using
+        binding.toggleButton.setOnCheckedChangeListener(toggleBtnOnCheckedChange)
+
+        // Switch Button Using
+        binding.switchBtn.setOnCheckedChangeListener(switchBtnOnCheckedChange)
+
+        // CheckBox Using
+        binding.checkBox.setOnCheckedChangeListener(chckBoxOnCheckedChange)
     }
+
+
+
     var buttonAlertOnClick = View.OnClickListener{
         Toast.makeText(this, "Step2", Toast.LENGTH_LONG).show()
     }
@@ -33,5 +47,22 @@ class MainActivity2 : AppCompatActivity() {
     /// From xml
     fun buttonAlertStep3(view: View) {
         Toast.makeText(this, "Step3", Toast.LENGTH_LONG).show()
+    }
+
+    // Toggle
+    var toggleBtnOnCheckedChange = CompoundButton.OnCheckedChangeListener{button: CompoundButton, isChecked: Boolean ->
+        Toast.makeText(this, "Toggle status: $isChecked", Toast.LENGTH_LONG).show()
+    }
+
+    //Switch
+    var switchBtnOnCheckedChange= CompoundButton.OnCheckedChangeListener{button: CompoundButton, isChecked: Boolean ->
+        Toast.makeText(this, "Switch status: $isChecked", Toast.LENGTH_LONG).show()
+
+        binding.toggleButton.visibility = if(isChecked) View.VISIBLE else View.INVISIBLE
+    }
+
+    // CheckBox
+    var chckBoxOnCheckedChange = CompoundButton.OnCheckedChangeListener{button: CompoundButton, isChecked: Boolean ->
+        Toast.makeText(this, "CheckBox Status: $isChecked", Toast.LENGTH_LONG).show()
     }
 }
